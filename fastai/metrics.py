cc446f31d1ca1addadb60cc176bf35951d4e295b
hlibbabii <hlibbabii@gmail.com>
2018-06-08 14:43:17 +0200

    add top_k accuracy metric

--- fastai/metrics.py
+++ fastai/metrics.py
@@ -6,6 +6,41 @@ from .torch_imports import *
 # *_np numpy preds/log_preds
 #
 
+def top_k(gen_preds_targs, n_batches, ks, cat):
+    assert n_batches
+    assert ks
+    assert cat <= len(ks)
+
+    sorted_ks = sorted([k * -1 for k in ks])
+    min_k = sorted_ks[0]
+    batch_count = 0
+    count_true = [0] * len(ks)
+    count_all=0
+    examples = []
+    tqdm1 = tqdm(gen_preds_targs, leave=False, total=n_batches)
+    for pred, targ, input in tqdm1:
+        bs = input[0].shape[1]
+        bptt = input[0].shape[0]
+        top_k_ind = np.argpartition(pred, sorted_ks, 1)[:,min_k:]
+        for i_in_batch, (pr, t) in enumerate(zip(top_k_ind, targ)):
+            for ind, k in enumerate(sorted_ks):
+                if t in pr[k:]:
+                    count_true[ind] += 1
+                else:
+                    if ind == (len(ks) - cat) and len(examples) < 100 and i_in_batch // bs > int(0.75 * bptt):
+                        examples.append((input[0][:, i_in_batch % bs], i_in_batch // bs, pr[k:], t))
+                    break
+
+            count_all +=1
+        batch_count += 1
+        temporary_result = {f"top{str(-k)}": f"{float(c)/count_all:.3}" for k, c in zip(sorted_ks, count_true)}
+        tqdm1.set_postfix(**temporary_result)
+    return temporary_result, examples
+
+def accuracy_np(preds, targs):
+    preds = np.argmax(preds, 1)
+    return (preds==targs).mean()
+
 def accuracy(preds, targs):
     preds = torch.max(preds, dim=1)[1]
     return (preds==targs).float().mean()
