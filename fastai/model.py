2d79c12dfc0fff0cd2493458e2853395a8178c71
hlibbabii <hlibbabii@gmail.com>
2018-11-16 00:51:52 +0100

    hlibbabii/log-recommender#39: draft implementation of entropy for subwrds

--- fastai/model.py
+++ fastai/model.py
@@ -248,18 +248,77 @@ def one_hot(idx, size, cuda=False):
     return v
 
 
-def validate_with_cache(stepper, dl, metrics, text_field, theta=2, lambdah=0.2, window = 1000):
+separators = ['<num_sep>', '<sc_sep>', '<cc_sep>', '<us_sep>']
+capital_letter_token = '<capital>'
+
+
+def get_curr_seq(seq, ps, current_target, current_p, text_field):
+    if not seq:  # only the beginning:
+        return None, None, [current_target], [current_p]
+
+    last_seq_word = text_field.vocab.itos[seq[-1][0]]
+    current_target_word = text_field.vocab.itos[current_target[0]]
+
+    if current_target_word == capital_letter_token:
+        return None, None, seq, ps
+
+    if (current_target_word not in separators) and (last_seq_word not in separators):
+        return seq, ps, [current_target], [current_p]
+    elif (current_target_word in separators) != (last_seq_word in separators):
+        # ongoing split word
+        return None, None, seq + [current_target], ps + [current_p]
+    else:
+        # current_target in separators and seq[-1] not separators
+        raise AssertionError(f"Two separators in a row are not: {last_seq_word}, {current_target_word}")
+
+
+sep_start_new = '`s'
+sep_end_new = 's`'
+capital_letter_token_new = '`C'
+
+
+def get_curr_seq_new(seq, ps, current_target, current_p, text_field):
+    current_target_word = text_field.vocab.itos[current_target[0]]
+    if current_target_word == capital_letter_token_new:
+        return None, None, seq, ps
+
+    if not seq:
+        if current_target_word == sep_start_new:
+            return None, None, [current_target], [current_p]
+        elif current_target_word == sep_end_new:
+            raise AssertionError(f"Split end separator without split start")
+        else:
+            return [current_target], [current_p], [], []
+    else:
+        if current_target_word == sep_start_new:
+            raise AssertionError(f"Second split start separator")
+        elif current_target_word == sep_end_new:
+            return seq + [current_target], ps + [current_p], [], []
+        else:
+            return [], [], seq + [current_target], ps + [current_p]
+
+
+def validate_with_cache(stepper, dl, metrics, text_field, theta=2, lambdah=0.0, window=1000):
     logging.info(f"Using theta: {theta}, lambdah: {lambdah}")
-    batch_cnts, losses, res = [], [], []
+    bptts, losses, res = [], [], []
+    seq = []
+    ps = []
+    seqs_in_batch_list = []
     stepper.reset(False)
     with no_grad_context():
         next_word_history = None
         pointer_history = None
         for (*x, y) in iter(dl):
+            # x - [Variable(x1,x2, ... xn)]
+            # y - Variable(x2...,xn, xn+1)
             batch_size = x[0].size(1)
             if batch_size != 1:
-                raise ValueError("For now only batch size 1 is supported for validation with cache")
+                raise ValueError("For now only batch v  vfrsize 1 is supported for validation with cache")
             preds, raw_outputs, l, targets = stepper.evaluate_with_cache(VV(x), VV(y))
+            # preds [bptt x vocab size] Variable
+            # raw_outputs [Variables[bptt x batch size x layer size], ...] - list, size: number of layers
+            # l - cross entropy Variable [batch size]
+            # targets [bptt]
             ntokens = preds.size(1)
             rnn_out = raw_outputs[-1].squeeze(1)  # from last layer
             output_flat = preds.view(-1, ntokens)
@@ -273,7 +332,9 @@ def validate_with_cache(stepper, dl, metrics, text_field, theta=2, lambdah=0.2, 
                 [pointer_history, Variable(rnn_out.data)], dim=0)
 
             loss = 0
+            seqs_in_batch = 0
             softmax_output_flat = torch.nn.functional.softmax(output_flat)
+            bptts.append(softmax_output_flat.size(0))
             preds_with_cache = []
             for idx, vocab_loss in enumerate(softmax_output_flat):
                 p = vocab_loss
@@ -285,35 +346,52 @@ def validate_with_cache(stepper, dl, metrics, text_field, theta=2, lambdah=0.2, 
                     ptr_dist = (ptr_attn.expand_as(valid_next_word) * valid_next_word).sum(0).squeeze()
                     p = lambdah * ptr_dist + (1 - lambdah) * vocab_loss
                     index = targets[idx].data[0]
-                    logging.info(f"========================================")
-                    logging.info(f"Actual word: {text_field.vocab.itos[index]}")
-                    logging.info("Result:")
-                    vals, indices = torch.topk(p, 3)
-                    for c, i in zip(vals, indices):
-                        logging.info(f"{text_field.vocab.itos[i.data[0]]} ({c.data[0]})")
-                    logging.info("From cache:")
-                    vals, indices = torch.topk(ptr_dist, 3)
-                    for c,i in zip(vals, indices):
-                        logging.info(f"{text_field.vocab.itos[i.data[0]]} ({c.data[0]})")
-                    logging.info("From nn:")
-                    vals, indices = torch.topk(vocab_loss, 3)
-                    for c,i in zip(vals, indices):
-                        logging.info(f"{text_field.vocab.itos[i.data[0]]} ({c.data[0]})")
+                    # logging.info(f"========================================")
+                    # logging.info(f"Actual word: {text_field.vocab.itos[index]}")
+                    # logging.info("Result:")
+                    # vals, indices = torch.topk(p, 3)
+                    # for c, i in zip(vals, indices):
+                    #     logging.info(f"{text_field.vocab.itos[i.data[0]]} ({c.data[0]})")
+                    # logging.info("From cache:")
+                    # vals, indices = torch.topk(ptr_dist, 3)
+                    # for c,i in zip(vals, indices):
+                    #     logging.info(f"{text_field.vocab.itos[i.data[0]]} ({c.data[0]})")
+                    # logging.info("From nn:")
+                    # vals, indices = torch.topk(vocab_loss, 3)
+                    # for c,i in zip(vals, indices):
+                    #     logging.info(f"{text_field.vocab.itos[i.data[0]]} ({c.data[0]})")
                 ###
-                target_loss = p[targets[idx].data]
-                loss += (-torch.log(target_loss)).data[0]
+                current_target = targets[idx].data
+                current_p = p[current_target]
+                curr_seq, current_ps, seq, ps = get_curr_seq(seq, ps, current_target, current_p, text_field)
+                if curr_seq is not None:
+                    for pp in current_ps:
+                        loss += (-torch.log(pp)).data[0]
+                    seqs_in_batch += 1
+                    if len(current_ps) > 0:
+                        logging.info("".join(map(lambda x: text_field.vocab.itos[x[0]], curr_seq)))
+                        lss = list(map(lambda x: (-torch.log(x)).data[0], current_ps))
+                        logging.info("" + str(list(map(lambda x: f'{x:.3f}', lss))) + f'= {sum(lss):.3f}')
+                        logging.info("============================")
                 preds_with_cache.append(p)
-            loss = loss / softmax_output_flat.size(0)
-            losses.append(to_np(V(loss)))
             ###
-            #hidden = repackage_hidden(hidden)
+            # hidden = repackage_hidden(hidden)
             next_word_history = next_word_history[-window:]
             pointer_history = pointer_history[-window:]
-
-            batch_cnts.append(batch_size)
             res.append([f(torch.stack(preds_with_cache).data, y) for f in metrics])
 
-    return [np.average(losses, 0, weights=batch_cnts)] + list(np.average(np.stack(res), 0, weights=batch_cnts))
+            if seqs_in_batch > 0:
+                loss = loss / seqs_in_batch
+                losses.append(to_np(V(loss)))
+
+                seqs_in_batch_list.append(seqs_in_batch)
+
+        for pp in ps:
+            loss += (-torch.log(pp)).data[0]
+        losses.append(to_np(V(loss)))
+        seqs_in_batch_list.append(1)
+
+    return [np.average(losses, 0, weights=seqs_in_batch_list)] + list(np.average(np.stack(res), 0, weights=bptts))
 
 
 def validate(stepper, dl, metrics, seq_first=False):
